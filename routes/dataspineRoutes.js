9561f1de56f577601b1c27bac17bb7f11824aeb1
hesamb1981 <hesam.b1981@gmail.com>
2025-12-01 20:11:32 +0000

    Update dataspineRoutes.js

--- routes/dataspineRoutes.js
+++ routes/dataspineRoutes.js
@@ -1,9 +1,12 @@
-// routes/dataSpineRoutes.js
 import express from "express";
-import { dataSpineHandler } from "../controllers/dataSpineController.js";
+import dataspineController from "../controllers/dataspineController.js";
 
 const router = express.Router();
 
-router.post("/generate", dataSpineHandler);
+// Demo
+router.post("/demo", dataspineController.demo);
+
+// Live
+router.post("/live", dataspineController.live);
 
 export default router;
