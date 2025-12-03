ee013fd578d2d865402e1c2a09816b31ce46330e
hesamb1981 <hesam.b1981@gmail.com>
2025-12-03 19:58:47 +0000

    Update aoieRoutes.js

--- routes/aoieRoutes.js
+++ routes/aoieRoutes.js
@@ -3,13 +3,13 @@ import { debugAoie, runAoie } from "../controllers/aoieController.js";
 
 const router = express.Router();
 
-// Debug route (GET - برای چک کردن سلامت AOIE)
+// Debug route  (GET)
 router.get("/debug", debugAoie);
 
-// Run route (GET - برای تست ساده در مرورگر)
+// Run route - GET برای تست ساده در مرورگر
 router.get("/run", runAoie);
 
-// Run route (POST - برای تست با ReqBin و سایر کلاینت‌ها)
+// Run route - POST برای ReqBin و فرانت‌اند
 router.post("/run", runAoie);
 
 export default router;
